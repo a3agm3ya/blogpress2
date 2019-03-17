14082cc645c7439ffde62d0b545df06f538b1ff4
a3agm3ya <a3agm3ya@gmail.com>
2019-03-17 11:18:03 +0530

    bp mfa

---
+++ app/src/main/java/com/example/blogpress/MainActivity.java
@@ -0,0 +1,102 @@
+package com.example.blogpress;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+
+public class MainActivity extends AppCompatActivity  {
+    EditText email;
+    EditText Password;
+    TextView newuser;
+    Button register;
+    ProgressDialog progressDialog;
+    FirebaseAuth firebaseAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        firebaseAuth=FirebaseAuth.getInstance ();
+        if(firebaseAuth.getCurrentUser ()!=null)
+        {
+           // Toast.makeText ( MainActivity.this , "Logged in.." , Toast.LENGTH_SHORT ).show ( );
+            finish();
+            startActivity(new Intent(getApplicationContext(), profile.class));
+        }
+
+
+        progressDialog=new ProgressDialog ( this );
+        email=findViewById(R.id.edttid);
+        Password=findViewById(R.id.edttpassword);
+        newuser=findViewById(R.id.txt1);
+        register=findViewById(R.id.bt1);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v==register)
+                { register();
+                }
+                if (v==newuser) {
+                    startActivity(new Intent(getApplicationContext(), loginpage.class));
+                }
+                }
+        });
+        newuser.setOnClickListener ( new View.OnClickListener ( ) {
+            @Override
+            public void onClick(View v) {
+                if(v==newuser)
+                {startActivity(new Intent(getApplicationContext(), loginpage.class));}
+            }
+        } );
+    }
+    public void register() {
+        String Email = email.getText().toString().trim();
+        String password  = Password.getText().toString().trim();
+
+        if(TextUtils.isEmpty(Email)){
+            Toast.makeText(MainActivity.this, "Enter Email ID", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(TextUtils.isEmpty(password)){
+            Toast.makeText(MainActivity.this, "Enter Password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        progressDialog.setMessage("Registering Please Wait...");
+        progressDialog.show();
+        firebaseAuth.createUserWithEmailAndPassword(Email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        //checking if success
+                        if(task.isSuccessful()) {
+                            Toast.makeText ( MainActivity.this , "Registered..." , Toast.LENGTH_SHORT ).show ( );
+                            finish ( );
+                            startActivity ( new Intent ( getApplicationContext ( ) , profile.class ) );
+
+                        }
+                        else{
+                            //display some message here
+                            Toast.makeText(MainActivity.this,"Registration Error",Toast.LENGTH_LONG).show();
+                        }
+                        progressDialog.dismiss();
+                    }
+                });
+    }
+
+}
+
---
+++ app/src/main/java/com/example/blogpress/loginpage.java
@@ -0,0 +1,103 @@
+package com.example.blogpress;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class loginpage extends AppCompatActivity {
+    TextView txtnew;
+    EditText edttemail;
+    EditText edttpassword;
+    Button btlogin;
+    ProgressDialog progressDialog;
+    FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loginpage);
+        firebaseAuth=FirebaseAuth.getInstance ();
+        if(firebaseAuth.getCurrentUser ()!=null)
+        {
+            Toast.makeText ( loginpage.this , "Logged in.." , Toast.LENGTH_SHORT ).show ( );
+            finish();
+            startActivity(new Intent(getApplicationContext(), profile.class));
+        }
+
+        progressDialog=new ProgressDialog ( this );
+
+        txtnew=findViewById ( R.id.txt1 );
+        edttemail=findViewById ( R.id.edttid );
+        edttpassword=findViewById ( R.id.edttpassword );
+        btlogin=findViewById ( R.id.bt1 );
+        btlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v == btlogin) {
+                    login ( );
+                }
+                if (v == txtnew) {
+                    startActivity ( new Intent ( getApplicationContext ( ) , MainActivity.class ) );
+                }
+            }
+            });
+        txtnew.setOnClickListener ( new View.OnClickListener ( ) {
+            @Override
+            public void onClick(View v) {
+                if (v == txtnew) {
+                    startActivity ( new Intent ( getApplicationContext ( ) , MainActivity.class ) );
+                }
+            }
+        } );
+
+    }
+    public void login(){
+
+        String Email = edttemail.getText().toString().trim();
+        String password  = edttpassword.getText().toString().trim();
+
+        if(TextUtils.isEmpty(Email)){
+            Toast.makeText(loginpage.this, "Enter Email ID", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(TextUtils.isEmpty(password)){
+            Toast.makeText(loginpage.this, "Enter Password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        progressDialog.setMessage("Logging You in Please Wait...");
+        progressDialog.show();
+        firebaseAuth.createUserWithEmailAndPassword(Email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult> () {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                progressDialog.dismiss ();
+                //checking if success
+                if(task.isSuccessful()){
+                    Toast.makeText ( loginpage.this , "Logged in.." , Toast.LENGTH_SHORT ).show ( );
+                    finish();
+                    startActivity(new Intent(getApplicationContext(), profile.class));
+
+                }
+                else{
+                    //display some message here
+                    Toast.makeText(loginpage.this,"Logging Error",Toast.LENGTH_LONG).show();
+                }
+                progressDialog.dismiss();
+            }
+        });
+    }
+}
